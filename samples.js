9f98d5eaa1b486be52cb76207ce1aa29fef5cc5a
Adharsh R <adharshrajeeev2000@gmail.com>
2023-01-26 10:56:08 +0530

    binary search added

--- samples.js
+++ samples.js
@@ -1,14 +1,26 @@
-var node1={
-data:50
+
+
+function binarySearch(arr,target){
+    
+    let start=0;
+    let end=arr.length-1;
+    
+   while(start<=end){
+    let middle=Math.floor((start+end)/2);
+
+    if(target ===  arr[middle]) return `value found at position  ${arr[middle]}`
+ 
+
+    if(target < arr[middle]){
+        end=middle-1;
+        
+    }else{
+        start=middle+1;
+    }
+    
+ }
+ return `value not found`
 }
 
-node2={
-    data:60
-}
+console.log(binarySearch([1,2,3,4,5],5))
 
-console.log(node1)
-
-
-node1.next=node2;
-
-console.log(node1)
