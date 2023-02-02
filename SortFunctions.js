11216fc2d45e92b0d7dc8ad0fe9ad98680d3dd81
Adharsh R <adharshrajeeev2000@gmail.com>
2023-02-02 06:36:07 +0530

    merge sort added

--- SortFunctions.js
+++ SortFunctions.js
@@ -1,4 +1,6 @@
 var newArray=[2, 8, 1, 3, 6, 7, 5, 4]
+var array1=[1,10,7];
+var array2=[3,5,2];
 
 function bubbleSort(arr){
     for(var i=0;i<arr.length-1;i++){
@@ -36,14 +38,53 @@ function selectionSort(arr){
 
 function insertionSort(arr){
     for(var i=1;i<arr.length;i++){
-        key=arr[i];
+       var key=arr[i];
         for(var j=i-1;arr[j] > key && j>=0;j--){
             arr[j+1]=arr[j]
         }
         arr[j+1]=key
     }
 
-    return arr
+    return arr;
+}
+ 
+// console.log(insertionSort(newArray))
+
+const merge=(arr1,arr2)=>{
+    let result=[];
+    let i=0;
+    let j=0;
+    
+    while(i< arr1.length && j < arr2.length){
+        if(arr1[i] <= arr2[j]){
+            result.push(arr1[i]);
+            i++
+        }else{
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while(i <arr1.length){
+        result.push(arr1[i]);
+        i++
+    }
+    while(j <arr2.length){
+        result.push(arr2[j]);
+        j++;
+    }
+    return result
 }
 
-console.log(insertionSort(newArray))
+function mergeSort(arr){
+
+    if(arr.length <= 1) return arr
+
+    let middle=Math.floor(arr.length/2);
+    let left=mergeSort(arr.slice(0,middle));
+    let right=mergeSort(arr.slice(middle));
+
+    return merge(left,right)
+}
+
+console.log(mergeSort([1]))
