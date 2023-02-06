be0bae9bdd09f5aea93bf7e44af8daf8a3daef8a
Adharsh R <adharshrajeeev2000@gmail.com>
2023-02-06 06:38:18 +0530

    workouts added

---
+++ allSortFunctions.js
@@ -0,0 +1,53 @@
+function bubbleSort(arr){
+    
+   for(var i=0;i<arr.length-1;i++){
+    var first=arr[i]
+    for(var j=i+1;j<arr.length;j++){
+    var second=arr[j]
+    
+        if(arr[i] > arr[j]){
+            let temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+   }
+   return arr;
+}
+// console.log(bubbleSort([10,2,8,9,4,5,1]));
+
+function selectionSort(arr){
+    for(var i=0;i<arr.length;i++){
+        let lowest=i;
+        for(j=i+1;j<arr.length;j++){
+            if(arr[lowest] > arr[j]){
+                lowest=j
+            }
+        }
+        var temp=arr[i];
+        arr[i]=arr[lowest];
+        arr[lowest]=temp
+    }
+
+    return arr;
+}
+
+// console.log(selectionSort([10,2,8,9,4,5,1]))
+
+
+function insertionSort(arr){
+    for(var i=1;i<arr.length;i++){
+        var key=arr[i];
+
+         for(var j=i-1;arr[j] > key && j>=0;j--){
+             arr[j+1]=arr[j]
+             var neqarr=arr
+         }
+         arr[j+1]=key;
+         var newww=arr
+     }
+ 
+     return arr;
+}
+
+console.log(insertionSort([10,2,8,9,4,5,1]))
---
+++ workouts.js
@@ -0,0 +1,78 @@
+function bubblesort(arr){
+    for(var i=0;i<arr.length-1;i++){
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[i] > arr[j]){
+                var temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp
+            }
+        }
+    }
+
+    return arr;
+}
+// console.log(bubblesort([10,2,6,4,8,3,9,7,1]))
+
+
+function selectionSort(arr){
+    for(var i=0;i<arr.length;i++){
+        var lowest=i;
+
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[lowest] >  arr[j]){
+                lowest=j;
+            }
+        }
+
+        var temp=arr[i];
+        arr[i]=arr[lowest];
+        arr[lowest]=temp
+
+    }
+
+    return arr
+}
+
+
+// console.log(selectionSort([10,2,6,4,8,3,9,7,1]))
+
+
+function insertionSort(arr){
+    for(var i=1;i<arr.length;i++){
+        var key=arr[i];
+        var arry=arr
+        for(j=i-1;j>=0 && arr[j] > key;j--){
+            arr[j+1]=arr[j]
+            var inside=arr
+        }
+        arr[j+1]=key;
+        var arry2=arr
+
+    }
+    return arr
+}
+
+
+// console.log(insertionSort([10,6,7,4,2]))
+
+function quickSort(arr){
+    if(arr.length < 2) return arr;
+
+    let pivot=arr[arr.length-1];
+    let left=[];
+    let right=[];
+
+    for(var i=0;i<arr.length-1;i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i])
+        }
+    }
+    let newleft=left;
+    let newRiht=right
+
+    return [...quickSort(left),pivot,...quickSort(right)]
+}
+
+console.log(quickSort([10,6,4,7,2]))
